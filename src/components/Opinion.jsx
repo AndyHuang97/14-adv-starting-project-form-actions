6c3f445c07829c61c21e1129755db15825a25571
Andrea Huang <huangandrea@hotmail.it>
2025-05-02 22:19:54 +0200

    Sending an HTTP Request via a Form Action

--- src/components/Opinion.jsx
+++ src/components/Opinion.jsx
@@ -1,10 +1,17 @@
+import { useContext } from "react";
+
+import { OpinionsContext } from "../store/opinions-context.jsx";
+
+
 export function Opinion({ opinion: { id, title, body, userName, votes } }) {
-  function upvoteAction() {
-    console.log("UPVOTE");
+  const { upvoteOpinion, downvoteOpinion } = useContext(OpinionsContext);
+
+  async function upvoteAction() {
+    await upvoteOpinion(id);
   }
 
-  function downvoteAction() {
-    console.log("DOWNVOTE");
+  async function downvoteAction() {
+    await downvoteOpinion(id);
   }
 
   return (
@@ -14,22 +21,22 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
         <p>Shared by {userName}</p>
       </header>
       <p>{body}</p>
-      <form className="votes">
+      <form className='votes'>
         <button formAction={upvoteAction}>
           <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
           >
-            <rect width="18" height="18" x="3" y="3" rx="2" />
-            <path d="m16 12-4-4-4 4" />
-            <path d="M12 16V8" />
+            <rect width='18' height='18' x='3' y='3' rx='2' />
+            <path d='m16 12-4-4-4 4' />
+            <path d='M12 16V8' />
           </svg>
         </button>
 
@@ -37,19 +44,19 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
 
         <button formAction={downvoteAction}>
           <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
           >
-            <rect width="18" height="18" x="3" y="3" rx="2" />
-            <path d="M12 8v8" />
-            <path d="m8 12 4 4 4-4" />
+            <rect width='18' height='18' x='3' y='3' rx='2' />
+            <path d='M12 8v8' />
+            <path d='m8 12 4 4 4-4' />
           </svg>
         </button>
       </form>
